20fe063e0d9be25fcdc196fba0ecc7506f25adf8
Vaibhav-gits <vaibhavvdy@gmail.com>
2026-05-13 17:19:06 +0530

    updated and error solve

--- my-portfolio/src/components/sections/Contact.jsx
+++ my-portfolio/src/components/sections/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
   setIsSubmitting(true);
 
   try {
-    const response = await fetch("http://localhost:5000/send-email", {
+    const response = await fetch("https://vaibhav-vaidya.vercel.app/send-email", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
