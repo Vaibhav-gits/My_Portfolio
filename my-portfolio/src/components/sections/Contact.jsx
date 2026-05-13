2e79947aa567514d84af97e34c1eda7906872c5b
Vaibhav-gits <vaibhavvdy@gmail.com>
2026-05-13 17:22:17 +0530

    updated

--- my-portfolio/src/components/sections/Contact.jsx
+++ my-portfolio/src/components/sections/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
   setIsSubmitting(true);
 
   try {
-    const response = await fetch("https://vaibhav-vaidya.vercel.app/send-email", {
+    const response = await fetch("https://vaibhav-vaidya.vercel.app", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
